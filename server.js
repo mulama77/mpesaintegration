57ce21b3af8ac3769ce1ac4dc6fbca682c14b60c
Kevin Mulama <kmmulama63@gmail.com>
2019-07-31 16:51:23 +0300

    started adding the remaining mpesa api's

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.use(bodyParser.json())
 app.use(morgan('combined', { stream: winston.stream }));
 
 // listen to api requests
+
+///////////////////////////////////////////////////////////
+// lipa na mpesa api requests
+///////////////////////////////////////////////////////////
 app.post('/mpesastkpush', function (req, res) {
     
     // credentials used in executing the mpesa api
@@ -76,6 +80,402 @@ app.post('/mpesastkpush', function (req, res) {
     });
     
    console.log("Waiting for the next request!");
+})
+
+///////////////////////////////////////////////////////////
+// lipanampesa callback
+///////////////////////////////////////////////////////////
+app.post('/mpesacallback', function (req, res) {
+    
+ console.log("Waiting for the next request!");
+})
+
+///////////////////////////////////////////////////////////
+// lipa na mpesa query requests
+///////////////////////////////////////////////////////////
+app.post('/mpesastkquery', function (req, res) {
+    
+  // credentials used in executing the mpesa api
+  const credentials = {
+  client_key: Constants.client_key,
+  client_secret: Constants.client_secret,
+  initiator_password: Constants.initiator_password,
+  certificatepath: Constants.certificatepath
+  };
+  
+  // the mpesa environnment this api is pointing to
+  const environment = Constants.environment;
+
+  // create a new instance of the api
+  const mpesa = new Mpesa(credentials, environment);
+
+  // invoking the mpesa api
+  mpesa
+  .lipaNaMpesaQuery({
+    BusinessShortCode: 123456,
+    CheckoutRequestID: "Checkout Request ID",
+    passKey: "Lipa Na Mpesa Pass Key"
+  })
+  .then(response => {
+  //Do something with the response
+  //eg
+  console.log(response);
+  /*
+  logger.log({
+      level: 'info',
+      message: response
+    }); */
+  res.end( JSON.stringify(response));
+  })
+  .catch(error => {
+  //Do something with the error;
+  //eg
+  console.error("ERROR"+error);
+  /*logger.log({
+      level: 'error',
+      message: error
+    }); */
+  res.end( JSON.stringify(error));
+  });
+  
+ console.log("Waiting for the next request!");
+})
+
+///////////////////////////////////////////////////////////
+// c2b register
+// This API enables Paybill and Buy Goods merchants to 
+// integrate to M-Pesa and receive real time payments notifications.
+///////////////////////////////////////////////////////////
+app.post('/c2bregister', function (req, res) {
+    
+  // credentials used in executing the mpesa api
+  const credentials = {
+  client_key: Constants.client_key,
+  client_secret: Constants.client_secret,
+  initiator_password: Constants.initiator_password,
+  certificatepath: Constants.certificatepath
+  };
+  
+  // the mpesa environnment this api is pointing to
+  const environment = Constants.environment;
+
+  // create a new instance of the api
+  const mpesa = new Mpesa(credentials, environment);
+
+  // invoking the mpesa api
+  mpesa
+  .c2bregister({
+    ShortCode: "Short Code",
+    ConfirmationURL: "Confirmation URL",
+    ValidationURL: "Validation URL",
+    ResponseType: "Response Type"
+  })
+  .then(response => {
+  //Do something with the response
+  //eg
+  console.log(response);
+  /*
+  logger.log({
+      level: 'info',
+      message: response
+    }); */
+  res.end( JSON.stringify(response));
+  })
+  .catch(error => {
+  //Do something with the error;
+  //eg
+  console.error("ERROR"+error);
+  /*logger.log({
+      level: 'error',
+      message: error
+    }); */
+  res.end( JSON.stringify(error));
+  });
+  
+ console.log("Waiting for the next request!");
+})
+
+///////////////////////////////////////////////////////////
+// c2b simulate
+///////////////////////////////////////////////////////////
+app.post('/c2bsimulate', function (req, res) {
+    
+  // credentials used in executing the mpesa api
+  const credentials = {
+  client_key: Constants.client_key,
+  client_secret: Constants.client_secret,
+  initiator_password: Constants.initiator_password,
+  certificatepath: Constants.certificatepath
+  };
+  
+  // the mpesa environnment this api is pointing to
+  const environment = Constants.environment;
+
+  // create a new instance of the api
+  const mpesa = new Mpesa(credentials, environment);
+
+  // invoking the mpesa api
+  mpesa
+  .c2bsimulate({
+    ShortCode: 123456,
+    Amount: 1000 /* 1000 is an example amount */,
+    Msisdn: 254792123456,
+    CommandID: "Command ID" /* OPTIONAL */,
+    BillRefNumber: "Bill Reference Number" /* OPTIONAL */
+  })
+  .then(response => {
+  //Do something with the response
+  //eg
+  console.log(response);
+  /*
+  logger.log({
+      level: 'info',
+      message: response
+    }); */
+  res.end( JSON.stringify(response));
+  })
+  .catch(error => {
+  //Do something with the error;
+  //eg
+  console.error("ERROR"+error);
+  /*logger.log({
+      level: 'error',
+      message: error
+    }); */
+  res.end( JSON.stringify(error));
+  });
+  
+ console.log("Waiting for the next request!");
+})
+
+
+///////////////////////////////////////////////////////////
+// b2c requests
+// This API enables the company to send funds to the customer
+///////////////////////////////////////////////////////////
+app.post('/b2c', function (req, res) {
+    
+  // credentials used in executing the mpesa api
+  const credentials = {
+  client_key: Constants.client_key,
+  client_secret: Constants.client_secret,
+  initiator_password: Constants.initiator_password,
+  certificatepath: Constants.certificatepath
+  };
+  
+  // the mpesa environnment this api is pointing to
+  const environment = Constants.environment;
+
+  // create a new instance of the api
+  const mpesa = new Mpesa(credentials, environment);
+
+  // invoking the mpesa api
+  mpesa
+  .b2c({
+    InitiatorName: "Initiator Name",
+    Amount: 1000 /* 1000 is an example amount */,
+    PartyA: "Party A",
+    PartyB: "Party B",
+    QueueTimeOutURL: "Queue Timeout URL",
+    ResultURL: "Result URL",
+    CommandID: "Command ID" /* OPTIONAL */,
+    Occasion: "Occasion" /* OPTIONAL */,
+    Remarks: "Remarks" /* OPTIONAL */
+  })
+  .then(response => {
+  //Do something with the response
+  //eg
+  console.log(response);
+  /*
+  logger.log({
+      level: 'info',
+      message: response
+    }); */
+  res.end( JSON.stringify(response));
+  })
+  .catch(error => {
+  //Do something with the error;
+  //eg
+  console.error("ERROR"+error);
+  /*logger.log({
+      level: 'error',
+      message: error
+    }); */
+  res.end( JSON.stringify(error));
+  });
+  
+ console.log("Waiting for the next request!");
+})
+
+///////////////////////////////////////////////////////////
+// Transaction status
+// Transaction Status API checks the status of a B2C and C2B APIs transactions.
+///////////////////////////////////////////////////////////
+app.post('/b2candc2bTransactionStatus', function (req, res) {
+    
+  // credentials used in executing the mpesa api
+  const credentials = {
+  client_key: Constants.client_key,
+  client_secret: Constants.client_secret,
+  initiator_password: Constants.initiator_password,
+  certificatepath: Constants.certificatepath
+  };
+  
+  // the mpesa environnment this api is pointing to
+  const environment = Constants.environment;
+
+  // create a new instance of the api
+  const mpesa = new Mpesa(credentials, environment);
+
+  // invoking the mpesa api
+  mpesa
+  .accountBalance({
+    Initiator: "Initiator Name",
+    PartyA: "Party A",
+    IdentifierType: "Identifier Type",
+    QueueTimeOutURL: "Queue Timeout URL",
+    ResultURL: "Result URL",
+    CommandID: "Command ID" /* OPTIONAL */,
+    Remarks: "Remarks" /* OPTIONAL */
+  })
+  .then(response => {
+  //Do something with the response
+  //eg
+  console.log(response);
+  /*
+  logger.log({
+      level: 'info',
+      message: response
+    }); */
+  res.end( JSON.stringify(response));
+  })
+  .catch(error => {
+  //Do something with the error;
+  //eg
+  console.error("ERROR"+error);
+  /*logger.log({
+      level: 'error',
+      message: error
+    }); */
+  res.end( JSON.stringify(error));
+  });
+  
+ console.log("Waiting for the next request!");
+})
+
+///////////////////////////////////////////////////////////
+// Reversal
+// Reverses a B2C or C2B M-Pesa transaction.
+///////////////////////////////////////////////////////////
+app.post('/b2candc2bReversal', function (req, res) {
+    
+  // credentials used in executing the mpesa api
+  const credentials = {
+  client_key: Constants.client_key,
+  client_secret: Constants.client_secret,
+  initiator_password: Constants.initiator_password,
+  certificatepath: Constants.certificatepath
+  };
+  
+  // the mpesa environnment this api is pointing to
+  const environment = Constants.environment;
+
+  // create a new instance of the api
+  const mpesa = new Mpesa(credentials, environment);
+
+  // invoking the mpesa api
+  mpesa
+  .reversal({
+    Initiator: "Initiator",
+    TransactionID: "Transaction ID",
+    Amount: 1000 /* 1000 is an example amount */,
+    ReceiverParty: "Reciever Party",
+    ResultURL: "Result URL",
+    QueueTimeOutURL: "Queue Timeout URL",
+    CommandID: "Command ID" /* OPTIONAL */,
+    RecieverIdentifierType: 11 /* OPTIONAL */,
+    Remarks: "Remarks" /* OPTIONAL */,
+    Occasion: "Ocassion" /* OPTIONAL */
+  })
+  .then(response => {
+  //Do something with the response
+  //eg
+  console.log(response);
+  /*
+  logger.log({
+      level: 'info',
+      message: response
+    }); */
+  res.end( JSON.stringify(response));
+  })
+  .catch(error => {
+  //Do something with the error;
+  //eg
+  console.error("ERROR"+error);
+  /*logger.log({
+      level: 'error',
+      message: error
+    }); */
+  res.end( JSON.stringify(error));
+  });
+  
+ console.log("Waiting for the next request!");
+})
+
+///////////////////////////////////////////////////////////
+// account balance
+// The Account Balance API requests for the account balance of a shortcode.
+///////////////////////////////////////////////////////////
+app.post('/accountBalance', function (req, res) {
+    
+  // credentials used in executing the mpesa api
+  const credentials = {
+  client_key: Constants.client_key,
+  client_secret: Constants.client_secret,
+  initiator_password: Constants.initiator_password,
+  certificatepath: Constants.certificatepath
+  };
+  
+  // the mpesa environnment this api is pointing to
+  const environment = Constants.environment;
+
+  // create a new instance of the api
+  const mpesa = new Mpesa(credentials, environment);
+
+  // invoking the mpesa api
+  mpesa
+  .accountBalance({
+    Initiator: "Initiator Name",
+    PartyA: "Party A",
+    IdentifierType: "Identifier Type",
+    QueueTimeOutURL: "Queue Timeout URL",
+    ResultURL: "Result URL",
+    CommandID: "Command ID" /* OPTIONAL */,
+    Remarks: "Remarks" /* OPTIONAL */
+  })
+  .then(response => {
+  //Do something with the response
+  //eg
+  console.log(response);
+  /*
+  logger.log({
+      level: 'info',
+      message: response
+    }); */
+  res.end( JSON.stringify(response));
+  })
+  .catch(error => {
+  //Do something with the error;
+  //eg
+  console.error("ERROR"+error);
+  /*logger.log({
+      level: 'error',
+      message: error
+    }); */
+  res.end( JSON.stringify(error));
+  });
+  
+ console.log("Waiting for the next request!");
 })
 
   //server listener
